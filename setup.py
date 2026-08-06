b345ca982b1eb7958f7902d4c4e232f950742af4
giovannibrumana <giovanni.brumana@moviri.com>
2026-08-06 10:09:30 +0200

    Cryptography update to 50.0.0.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open_relative("README.rst") as f:
 
 requires = [
     "certifi",
-    "cryptography>=46.0.5,<50.0.0",
+    "cryptography>=50.0.0",
     "pyOpenSSL>=26.0.0,<27.0.0",
     "python-dateutil>=2.5.3,<3.0.0",
     "pytz>=2016.10",
